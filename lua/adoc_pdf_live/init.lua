d6c898d06338496988a941f27a59bd295d9fa360
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-04-27 17:08:34 +0200

    fix find command

--- lua/adoc_pdf_live/init.lua
+++ lua/adoc_pdf_live/init.lua
@@ -43,12 +43,12 @@ local function find_style()
     log("Looking for styles automatically")
 
     local cmd = string.format(
-        "find . \
-            -maxdepth 2 \
-            -regex '.*/%s' \
-            -regextype sed \
-            -print \
-            -quit",
+        "find ." ..
+            " -maxdepth 2" ..
+            " -regex '.*/%s'" ..
+            " -regextype sed" ..
+            " -print" ..
+            " -quit",
         adoc_pdf_live.options.style_regex
     )
     local found = exec(cmd, false)
